98806b057cbb72e35d296a79cdeddf55d1f87c59
Aniket Dhaniskar <74300927+blacksummer29@users.noreply.github.com>
2020-11-19 11:19:35 +0530

    Add files via upload

---
+++ volume_of_cube.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+ int side,volume;
+ 
+ printf("\n Enter the side of the cube:");
+ scanf("%d",&side);
+ volume=side*side*side;
+ printf("\n Volume of cube is %d",volume);
+ return 0;
+}
+
