9f6e67a0a04d7651af096d1b798410211c33349b
Aniket Dhaniskar <74300927+blacksummer29@users.noreply.github.com>
2020-11-19 11:18:37 +0530

    Add files via upload

---
+++ perimeter_of_square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+ int side,perimter;
+ 
+ printf("\n Enter the side of the square:");
+ scanf("%d",&side);
+ perimter=4*side;
+ printf("\n Perimter of square is %d",perimter);
+ return 0;
+}
